936b9304980a50ab4de6e478a300398394a62386
Sahil <leviack629@gmail.com>
2023-08-18 15:52:30 +0530

    mern blog

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    
+    const response =  await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    
+if (response.status !== 200 ) {
+  alert('Registration failed!')
+} else {
+  alert("Registration successful!")
+}
+
+    
+  }
+  return (
+    <>
+      <form action="" className="register" onSubmit={register}>
+        <h1>Register</h1>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button>Register</button>
+      </form>
+    </>
+  );
+};
+
+export default RegisterPage;
